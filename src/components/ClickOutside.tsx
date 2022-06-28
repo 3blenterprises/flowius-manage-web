5a561dcabc79dcd65fbc4022100af71febf71493
Menasi Ephrem <menasiephrem@gmail.com>
2022-06-28 13:59:34 +0300

    ✨  run pretier

--- src/components/ClickOutside.tsx
+++ src/components/ClickOutside.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 interface ICloseClickOutsideProps {
   onClose: (e?: any) => void;
@@ -13,23 +13,27 @@ const CloseClickOutside = ({
   className,
   onClick,
 }: ICloseClickOutsideProps) => {
+  const handleOutsideClick = useCallback(
+    (e: MouseEvent) => {
+      if (
+        ref.current &&
+        e.target instanceof HTMLElement &&
+        !ref.current.contains(e.target)
+      ) {
+        onClose(e);
+      }
+    },
+    [onClose]
+  );
+
   useEffect(() => {
     document.addEventListener("mousedown", handleOutsideClick);
     return () => {
       document.removeEventListener("mousedown", handleOutsideClick);
     };
-  }, []);
+  }, [handleOutsideClick]);
 
   const ref = useRef<HTMLDivElement>(null);
-  const handleOutsideClick = (e: MouseEvent) => {
-    if (
-      ref.current &&
-      e.target instanceof HTMLElement &&
-      !ref.current.contains(e.target)
-    ) {
-      onClose(e);
-    }
-  };
 
   return (
     <div onClick={onClick} className={className} ref={ref}>
