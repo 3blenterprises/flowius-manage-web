9214894773b8edbf819065f7c10ae3ae609034a5
Menasi Ephrem <menasiephrem@gmail.com>
2022-06-30 15:18:08 +0300

    🚧  Add Map

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,60 @@
-import { FC } from "react";
+import { FC, useCallback, useContext, useEffect, useState } from "react";
+import Map, { Marker, FullscreenControl } from "react-map-gl";
+
+import { ProjectContext } from "../context/ProjectContext";
+import { getProjectCases } from "../services/caseService";
+import { ICases } from "../services/orgTypes";
 
 const Home: FC = () => {
-  return <div>Home</div>;
+  const [cases, setCases] = useState<ICases[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const projectContext = useContext(ProjectContext);
+  const { id } = projectContext.selectedProject;
+
+  const pullCases = useCallback(async () => {
+    if (!id) return;
+    setLoading(true);
+    const cases = await getProjectCases(id);
+    setCases(cases);
+    setLoading(false);
+  }, [id]);
+
+  useEffect(() => {
+    pullCases();
+  }, [pullCases]);
+  return (
+    <div className="flex flex-col items-center w-full h-screen">
+      <div className="m-2 w-full h-1/2 flex flex-1 justify-start items-center">
+        Charts go here
+      </div>
+      <div className="m-2 w-4/5 h-1/2">
+        <Map
+          mapboxAccessToken={import.meta.env.VITE_MAP_BOX_TOKEN}
+          initialViewState={{
+            longitude: 39,
+            latitude: 9,
+            zoom: 8.75,
+          }}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+        >
+          <FullscreenControl />
+          {cases.map((c, i) => {
+            if (!c.geoLocation) return <div key={`${id}_${i}`}></div>;
+            const location = c.geoLocation.split("/");
+            return (
+              <Marker
+                key={`${id}_${i}`}
+                latitude={Number(location[0])}
+                longitude={Number(location[2])}
+                anchor="bottom"
+              />
+            );
+          })}
+        </Map>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
