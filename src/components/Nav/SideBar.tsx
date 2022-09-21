f5bdf23f8dc9d0db00c4313585500bf4ce09d97e
Elias Shemsu <echosierran@gmail.com>
2022-09-21 15:22:44 +0300

    Side bar UI approximated to the one in flowius pay dashboard

--- src/components/Nav/SideBar.tsx
+++ src/components/Nav/SideBar.tsx
@@ -1,37 +1,63 @@
+/* eslint-disable prettier/prettier */
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import MaterialIcon from "../MaterialIcon";
 
+const style_sidebar = {
+    
+}
+
 const SideBar: FC = () => {
   return (
-    <div className="w-60 hidden sm:flex bg-white shadow" aria-label="Sidebar">
+    // <div className="w-60 hidden sm:flex bg-white shadow" aria-label="Sidebar">
+    <div className="w-56 hidden sm:flex top-0 left-0 bg-cover bg-white shadow block ml-5 rounded bg-gradient-to-b from-[#1d8cf8] to-[#3358f4]/90" aria-label="Sidebar">
       <div className=" py-4 px-3 rounded ">
         <ul className="space-y-2">
           <li>
             <Link
               to="/"
-              className="flex items-center capitalize text-gray-500 font-bold p-2 text-base rounded-lg  hover:bg-gray-100 "
+              className="flex items-center capitalize text-gray-500 font-bold p-2 text-base rounded-lg"
             >
-              <MaterialIcon icon="home" outline={true} className="" />
-              <span className="ml-3">Dashboard</span>
+              <span className="ml-3 text-white">FLOWIUS MANAGE</span>
+            </Link>
+          </li>
+
+          <br className=""/>
+
+          <li>
+            <Link
+              to="/"
+              className="flex items-center capitalize text-gray-500 p-2 text-base rounded-lg  hover:bg-[#3358f4]/90"
+            >
+              <MaterialIcon icon="home" outline={true} className="text-white" />
+              <span className="ml-3 text-white">Dashboard</span>
             </Link>
           </li>
           <li>
             <Link
               to="/materials"
-              className="flex items-center text-gray-500 font-bold p-2 text-base rounded-lg  hover:bg-gray-100 "
+              className="flex items-center text-gray-500 p-2 text-base rounded-lg  hover:bg-[#3358f4]/90"
             >
-              <MaterialIcon icon="construction" outline={true} className="" />
-              <span className="ml-3">Materials</span>
+              <MaterialIcon icon="construction" outline={true} className="text-white" />
+              <span className="ml-3 text-white">Materials</span>
             </Link>
           </li>
           <li>
             <Link
               to="/cases"
-              className="flex items-center text-gray-500 font-bold p-2 text-base rounded-lg  hover:bg-gray-100 "
+              className="flex items-center text-gray-500 p-2 text-base rounded-lg  hover:bg-[#3358f4]/90"
             >
-              <MaterialIcon icon="content_paste" outline={true} className="" />
-              <span className="ml-3">Cases</span>
+              <MaterialIcon icon="content_paste" outline={true} className="text-white" />
+              <span className="ml-3 text-white">Cases</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/cases"
+              className="flex items-center text-gray-500 p-2 text-base rounded-lg  hover:bg-[#3358f4]/90"
+            >
+              <MaterialIcon icon="settings" outline={true} className="text-white" />
+              <span className="ml-3 text-white">Project Settings</span>
             </Link>
           </li>
         </ul>
